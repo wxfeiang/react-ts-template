97f948ee3837b954acf07e6dc210d3a8c819ace5
wxfeiang <wxfeiang@qq.com>
2026-01-24 23:11:26 +0800

    feat:  foter2

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,11 +2,9 @@ export default function footer() {
   return (
     <footer className="py-4  bg-main text-white">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 ">
-          <div className="flex flex-col gap-2 text-left sm:text-center">
-            <div className="">
-              <img src="https://picsum.photos/200" alt="" className="w-40 h-20" />
-            </div>
+        <div className="grid   gap-4 grid-cols-1 md:grid-cols-3 ">
+          <div className="flex flex-col gap-2 text-center">
+            <img src="https://picsum.photos/200" alt="" className="w-40 h-20 mx-auto " />
             <div>联系地址: 甘肃省兰州市</div>
             <div>联系电话: 123456789</div>
             <div>Email: 123456789@qq.com</div>
@@ -22,7 +20,7 @@ export default function footer() {
               </div>
             </div>
           </div>
-          <div>
+          <div className="">
             <div className="text-center">友情链接</div>
           </div>
         </div>
